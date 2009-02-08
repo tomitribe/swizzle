c6c76f2f788e2e8774c26dc7c5fc3b48744e2821
David Blevins <david.blevins@gmail.com>
2009-02-08 23:35:45 +0000

    SWIZZLE-33: error in lookingForToken strategy in DelimitedTokenReplacementInputStream

--- swizzle-stream/src/test/java/org/codehaus/swizzle/stream/DelimitedTokenReplacementInputStreamTest.java
+++ swizzle-stream/src/test/java/org/codehaus/swizzle/stream/DelimitedTokenReplacementInputStreamTest.java
@@ -30,77 +30,120 @@ public class DelimitedTokenReplacementInputStreamTest extends TestCase {
     StreamTokenHandler testTokenHandler = new TestTokenHandler();
 
     public void testTokenizingFilterInputStream() throws Exception {
+
+        // Symmetrical being end pairs
+        assertReplacement("{", "}");
+        assertReplacement("{{", "}}");
+        assertReplacement("{{{", "}}}");
+        assertReplacement("{{{{", "}}}}");
+        assertReplacement("{{{{{", "}}}}}");
+        assertReplacement("{{{{{{{{{{{{", "}}}}}}}}}}}}");
+
+        // Aymmetrical being end pairs
+        assertReplacement("{", "}}");
+        assertReplacement("{{", "}}}");
+        assertReplacement("{{{", "}}}}");
+        assertReplacement("{{{{", "}}}}}");
+        assertReplacement("{{{{{", "}}}}}}}}}}}}");
+        assertReplacement("{{", "}");
+        assertReplacement("{{{", "}}");
+        assertReplacement("{{{{", "}}}");
+        assertReplacement("{{{{{", "}}}}");
+        assertReplacement("{{{{{{{{{{{{", "}}}}}");
+
+
+        assertReplacement("BEGIN", "END");
+
+    }
+
+    private void assertReplacement(String b, String e) throws IOException {
         String original = "";
         String expected = "";
 
-        original = "some {FOO} text";
+        original = b + "FOO" + e + "abc";
+        expected = "appleabc";
+        swizzleAndAssert(original, expected, b, e);
+
+        original = "a" + b + "FOO" + e + "bc";
+        expected = "aapplebc";
+        swizzleAndAssert(original, expected, b, e);
+
+        original = "ab" + b + "FOO" + e + "c";
+        expected = "abapplec";
+        swizzleAndAssert(original, expected, b, e);
+
+        original = "abc" + b + "FOO" + e;
+        expected = "abcapple";
+        swizzleAndAssert(original, expected, b, e);
+
+        original = "some " + b + "FOO" + e + " text";
         expected = "some apple text";
-        swizzleAndAssert(original, expected);
+        swizzleAndAssert(original, expected, b, e);
 
-        original = "{FOO} text";
+        original = b + "FOO" + e + " text";
         expected = "apple text";
-        swizzleAndAssert(original, expected);
+        swizzleAndAssert(original, expected, b, e);
 
-        original = "some text {FOO}";
+        original = "some text " + b + "FOO" + e;
         expected = "some text apple";
-        swizzleAndAssert(original, expected);
+        swizzleAndAssert(original, expected, b, e);
 
-        original = "{FOO}";
+        original = b + "FOO" + e;
         expected = "apple";
-        swizzleAndAssert(original, expected);
+        swizzleAndAssert(original, expected, b, e);
 
-        original = "some {FOO}O text";
+        original = "some " + b + "FOO" + e + "O text";
         expected = "some appleO text";
-        swizzleAndAssert(original, expected);
+        swizzleAndAssert(original, expected, b, e);
 
         original = "some text";
         expected = "some text";
-        swizzleAndAssert(original, expected);
+        swizzleAndAssert(original, expected, b, e);
 
         original = "";
         expected = "";
-        swizzleAndAssert(original, expected);
+        swizzleAndAssert(original, expected, b, e);
 
         original = "some FO text";
         expected = "some FO text";
-        swizzleAndAssert(original, expected);
+        swizzleAndAssert(original, expected, b, e);
 
         original = "FO some text";
         expected = "FO some text";
-        swizzleAndAssert(original, expected);
+        swizzleAndAssert(original, expected, b, e);
 
         original = "some text FO";
         expected = "some text FO";
-        swizzleAndAssert(original, expected);
+        swizzleAndAssert(original, expected, b, e);
 
-        original = "some {FOO} {FOO} text";
+        original = "some " + b + "FOO" + e + " " + b + "FOO" + e + " text";
         expected = "some apple apple text";
-        swizzleAndAssert(original, expected);
+        swizzleAndAssert(original, expected, b, e);
 
-        original = "{FOO} text {FOO}";
+        original = b + "FOO" + e + " text " + b + "FOO" + e;
         expected = "apple text apple";
-        swizzleAndAssert(original, expected);
+        swizzleAndAssert(original, expected, b, e);
 
-        original = "{FOO}{FOO}";
+        original = b + "FOO" + e + b + "FOO" + e;
         expected = "appleapple";
-        swizzleAndAssert(original, expected);
+        swizzleAndAssert(original, expected, b, e);
 
-        original = "BAsome {FOO}O text";
+        original = "BAsome " + b + "FOO" + e + "O text";
         expected = "BAsome appleO text";
-        swizzleAndAssert(original, expected);
+        swizzleAndAssert(original, expected, b, e);
 
-        original = "{BAR}{FOO}{FOO}{BAR} text";
+        original = b + "BAR" + e + b + "FOO" + e + b + "FOO" + e + b + "BAR" + e + " text";
         expected = "orangeappleappleorange text";
-        swizzleAndAssert(original, expected);
+        swizzleAndAssert(original, expected, b, e);
 
-        original = "{BAR} some {FOO} {BAR} test {FOO}";
+        original = b + "BAR" + e + " some " + b + "FOO" + e + " " + b + "BAR" + e + " test " + b + "FOO" + e;
         expected = "orange some apple orange test apple";
-        swizzleAndAssert(original, expected);
+        swizzleAndAssert(original, expected, b, e);
     }
 
-    private void swizzleAndAssert(String original, String expected) throws IOException {
+    private void swizzleAndAssert(String original, String expected, String begin, String end) throws IOException {
         InputStream in = StreamUtils.stringToStream(original);
-        in = new DelimitedTokenReplacementInputStream(in, "{", "}", testTokenHandler);
+        in = new DelimitedTokenReplacementInputStream(in, begin, end, testTokenHandler);
         String actual = StreamUtils.streamToString(in);
 
         assertEquals(expected, actual);
